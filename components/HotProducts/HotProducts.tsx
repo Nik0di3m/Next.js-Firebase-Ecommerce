0398ea8933e4527eb11f1e793c6a730f755548d2
Nik0di3m <norbert.waleszczyk@icloud.com>
2021-09-03 14:00:17 +0200

    fix header

--- components/HotProducts/HotProducts.tsx
+++ components/HotProducts/HotProducts.tsx
@@ -4,7 +4,7 @@ const HotProducts = () => {
     return (
         <div className="py-6 lg:py-12">
             <h1 className="text-4xl">Hot Products</h1>
-            <h2>Bestsellers in our stash</h2>
+            <h2>{`Bestseller's especially for You `}</h2>
             <div className="grid grid-cols-1 justify-items-center md:grid-cols-2 lg:grid-cols-3 gap-3 mt-8">
                 <HotCard
                     price={200}
